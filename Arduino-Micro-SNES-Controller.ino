c36c748ba5b9be95f10bcc535ed020cfc7f33ada
Stuart Teasdale <stuart@drogna.org.uk>
2020-12-07 17:32:49 +0000

    Initial commit

---
+++ Arduino-Micro-SNES-Controller.ino
@@ -0,0 +1,123 @@
+//#include <Joystick2.h>
+#include "HID-Project.h"
+/** Creator: Anthony Burkholder
+ ** Date: Sept 24, 2016
+ ** Project: USBSNES Controller
+ ** Version: 0.0.1
+ **/
+
+/** PINS **/
+int DATA_CLOCK    = 2;
+int DATA_LATCH    = 4;
+int DATA_SERIAL   = 6;
+
+
+
+enum snes_controller {
+  SNES_B,
+  SNES_Y,
+  SNES_SELECT,
+  SNES_START,
+  SNES_UP,
+  SNES_DOWN,
+  SNES_LEFT,
+  SNES_RIGHT,
+  SNES_A,
+  SNES_X,
+  SNES_L,
+  SNES_R
+};
+int button_order[8] = {SNES_A,SNES_B,SNES_X,SNES_Y,SNES_L,SNES_R,SNES_START,SNES_SELECT}; 
+void setup () {
+//  Joystick[0].begin();
+//Serial.begin(115200);
+  Gamepad.begin();
+  Gamepad.releaseAll();
+  setupPins ();
+}
+
+void loop () {
+  
+  RXTXControllerData ();
+}
+
+void setupPins () {
+  /** Set DATA_CLOCK normally HIGH **/
+  pinMode (DATA_CLOCK, OUTPUT);
+  digitalWrite (DATA_CLOCK, HIGH);
+  
+  /** Set DATA_LATCH normally LOW **/
+  pinMode (DATA_LATCH, OUTPUT);
+  digitalWrite (DATA_LATCH, LOW);
+
+  /** Set DATA_SERIAL normally HIGH **/
+  pinMode (DATA_SERIAL, OUTPUT);
+  digitalWrite (DATA_SERIAL, HIGH);
+  pinMode (DATA_SERIAL, INPUT);  
+}
+
+void RXTXControllerData () {
+
+  /** Data store for current state of buttons **/
+  int buttons[12];
+  uint8_t dpad;
+  uint32_t b;
+  /** Latch for 12us **/
+  digitalWrite(DATA_LATCH, HIGH);
+  delayMicroseconds(12);
+  digitalWrite(DATA_LATCH, LOW);
+  delayMicroseconds(6);
+
+  /** Read data bit by bit from SR **/
+  for (int i = 0; i < 16; i++) {
+    digitalWrite (DATA_CLOCK, LOW);
+    delayMicroseconds (6);
+    if (i <= 11) {
+      buttons[i] = !digitalRead (DATA_SERIAL);
+    }
+    digitalWrite (DATA_CLOCK, HIGH);
+    delayMicroseconds (6);
+  }
+  
+  //Code the dpad
+  dpad = codeDPad(buttons[SNES_UP],buttons[SNES_DOWN],buttons[SNES_LEFT],buttons[SNES_RIGHT]);
+  //Code the Buttons
+  b=0;
+  for (int i=0; i < 8; i++) {
+    b |= (buttons[button_order[i]] << i);
+  }
+  /** Set Joystick state based on SNES input **/
+  Gamepad.dPad1(dpad);
+  Gamepad.buttons(b);
+  Gamepad.write();
+
+/*      
+    Serial.print(dpad);
+    Serial.print('|');
+    Serial.print(b);
+    Serial.print('|');  
+  for (int i = 0; i <= 11; i++) {
+
+    Serial.print(buttons[i]);
+    Serial.print(' ');
+  }
+  Serial.println(); */
+}
+
+uint8_t codeDPad( int u, int d, int l, int r ) {
+  if (u){
+    if (l){ return GAMEPAD_DPAD_UP_LEFT;}
+    else if (r){ return GAMEPAD_DPAD_UP_RIGHT;}
+    else { return GAMEPAD_DPAD_UP;}
+  } else if (d) {
+    if (l){ return GAMEPAD_DPAD_DOWN_LEFT;}
+    else if (r) { return GAMEPAD_DPAD_DOWN_RIGHT;}
+    else {return GAMEPAD_DPAD_DOWN;}
+  } else if (l) {
+    return GAMEPAD_DPAD_LEFT;
+  } else if (r) {
+    return GAMEPAD_DPAD_RIGHT;
+  }
+  
+  return -127;
+}
